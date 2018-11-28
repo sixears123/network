e4f3e3b15c7703f3eb780bc9ef242e2ede4454cb
jaeliu <3095066309@qq.com>
2018-11-28 19:33:53 +0800

    1.1

--- main.cpp
+++ main.cpp
@@ -1,73 +1,97 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <fstream>
+#define MAX 100
+#define max_value 1000
 using namespace std;
-const int maxint = 10000000;
-const int maxn = 1000;
-int x, y, z;
-int dis[maxn][maxn]; //表示两个点之间的距离
-int n, m;
-int next_[maxn][maxn]; //表示i到j的下一跳
-void floyd() {
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= n; ++j) {
-			next_[i][j] = j;
-		}
-	}//
-	for (int k = 1; k <= n; ++k) {   //枚举中间点k
-		for (int i = 1; i <= n; ++i) {   //枚举端点i
-			for (int j = 1; j <= n; ++j) {   //枚举端点j
-				if (dis[i][j] > (dis[i][k] + dis[k][j])) {
-					dis[i][j] = dis[i][k] + dis[k][j];
-					next_[i][j] = next_[i][k];
-				}
-			}
-		}
-	}
+void Floyd(int Array[MAX][MAX],int RouteNum,int cost[MAX][MAX],int NextRoute[MAX][MAX]){
+    for (int i = 1; i <RouteNum+1 ; ++i) {
+        for (int j = 1; j <RouteNum+1 ; ++j) {
+            cost[i][j]=Array[i][j];
+            NextRoute[i][j]=j;
+        }
+    }
+    for (int k = 1; k <RouteNum+1 ; ++k) {
+        for (int i = 1; i<RouteNum+1 ; ++i) {
+            for (int j = 1; j <RouteNum+1 ; ++j) {
+                if(cost[i][j]>(cost[i][k]+cost[k][j])){
+                    cost[i][j]=cost[i][k]+cost[k][j];
+                    NextRoute[i][j]=NextRoute[i][k];
+                }
+            }
+        }
+    }
 }
-int main() {
-	while (cin >> n >> m && n&&m) {
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				dis[i][j] = maxint;
-			}
-		}
-		for (int i = 1; i <= n; ++i) dis[i][i] = 0;
-		for (int i = 0; i < m; ++i) {
-			cin >> x >> y >> z;
-			dis[x][y] = dis[y][x] = z;
-		}
-		floyd();
-		for (int i = 1; i <= n; ++i) {
-			cout << "**********************************" << endl;
-			cout << "***     路由器" << i << "的路由表        ***" << endl;
-			cout << "***  目的     距离     下一跳  ***" << endl;
-			for (int j = 1; j <= n; ++j) {
-				if (j != i) cout<<"*     "<< j << "        " << dis[i][j] << "          " << next_[i][j] <<"      *"<< endl;
-			}
-			cout << "**********************************" << endl;
-			cout << endl;
-		}
-	}
-	return 0;
+void output(int RouteNum,int cost[MAX][MAX],int NextRoute[MAX][MAX]){
+    ofstream file;
+    file.open("Route.txt");
+    for (int i = 1; i <RouteNum+1 ; ++i) {
+        file<<"第"<<i<<"个路由表"<<"\r"<<"\n";
+        file<<"目的"<<"距离"<<"下一跳"<<"\r"<<"\n";
+        for (int j = 1; j <RouteNum+1; ++j) {
+            if(i!=j){
+                file<<j<<"   "<<cost[i][j]<<"    "<<NextRoute[i][j]<<"\r"<<"\n";
+            }
+        }
+    }
+    file.close();
 }
+int input(int Array[MAX][MAX]){
+    ifstream file;
+    int RouteNum,m,x,y,z;
+    file.open("db.txt");
+    file >> RouteNum >> m;
+    for (int i = 1; i <= RouteNum; ++i) {
+        for (int j = 1; j <= RouteNum; ++j) {
+            Array[i][j] =max_value;
+        }
+    }
+    for (int i = 1; i <= RouteNum; ++i) Array[i][i] = 0;
+    for (int i = 0; i < m; ++i) {
+        file >> x >> y >> z;
+        Array[x][y] = Array[y][x] = z;
+    }
+    file.close();
+    return RouteNum;
+}
+int AddRoute(int Array[MAX][MAX],int RouteNum){
+    int n,j,Dis;
+    RouteNum=RouteNum+1;
+    for (int i = 1; i <=RouteNum ; ++i) {
+        Array[RouteNum][i]=max_value;
+        Array[i][RouteNum]=max_value;
+    }
+    Array[RouteNum][RouteNum]=0;
+    cin>>n;
+    while(n--){
+        cin>>j>>Dis;
+        Array[RouteNum][j]=Dis;
+        Array[j][RouteNum]=Dis;
+    }
+    return RouteNum;
+}
+int DelRoute(int Array[MAX][MAX],int RouteNum,int code){
+    for (int i = code; i <RouteNum ; ++i) {
+        for (int j = 1; j <=RouteNum ; ++j) {
+            Array[i][j]=Array[i+1][j];
+        }
+    }
+    for (int j = code; j <RouteNum ; ++j) {
+        for (int i = 1; i <=RouteNum ; ++i) {
+            Array[i][j]=Array[i][j+1];
+        }
+    }
+    return RouteNum-1;
+}
+void AddEdge(int Array[MAX][MAX]){
+//
+}
+void DelEdge(){
 
-
-/*
-9 16
-1 2 2
-1 6 1
-1 7 1
-2 3 2
-2 4 4
-3 4 1
-3 6 3
-3 7 2
-4 5 1
-4 7 6
-4 8 5
-4 9 2
-5 6 5
-6 7 1
-7 8 3
-8 9 2
-*/
+}
+int main(){
+    int Array[MAX][MAX],cost[MAX][MAX],NextRoute[MAX][MAX],RouteNum;
+    RouteNum=input(Array);
+    Floyd(Array,RouteNum,cost,NextRoute);
+    output(RouteNum,cost,NextRoute);
+    return 0;
+}
