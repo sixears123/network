d25704873b97b58398af2f8906eed5b0ddd638d8
xbrx <2725281280@qq.com>
2018-12-01 16:21:12 +0800

    1

--- File.cpp
+++ File.cpp
@@ -9,14 +9,15 @@ using namespace std;
 void output(int RouteNum,int cost[1000][1000],int NextRoute[1000][1000]){
     ofstream file;
     file.open("Route.txt");
-    for (int i = 1; i <RouteNum+1 ; ++i) {
-        file<<"第"<<i<<"个路由表"<<"\r"<<"\n";
-        file<<"目的"<<"距离"<<"下一跳"<<"\r"<<"\n";
-        for (int j = 1; j <RouteNum+1; ++j) {
-            if(i!=j){
-                file<<j<<"   "<<cost[i][j]<<"    "<<NextRoute[i][j]<<"\r"<<"\n";
-            }
+    for (int i = 1; i <= RouteNum+1; ++i) {
+        file << "**********************************" <<"\r"<<"\n";
+        file << "***     路由器" << i << "的路由表        ***" <<"\r"<<"\n";
+        file << "***  目的     距离     下一跳  ***" <<"\r"<<"\n";
+        for (int j = 1; j <= RouteNum+1; ++j) {
+            if (j != i) file<<"*     "<< j << "        " << cost[i][j] << "          " << NextRoute[i][j] <<"      *"<<"\r"<<"\n";
         }
+        file << "**********************************" <<"\r"<<"\n";
+        file <<"\r"<<"\n";
     }
     file.close();
 }
