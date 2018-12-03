33607e2c35b75f8fe6aebef9ba8cce0e84305f6a
PG2819 <251158365@qq.com>
2018-12-03 19:37:16 +0800

    66

--- File.cpp
+++ File.cpp
@@ -14,7 +14,7 @@ void output(int RouteNum,int cost[1000][1000],int NextRoute[1000][1000]){
         file << "***     路由器" << i << "的路由表        ***" <<"\r"<<"\n";
         file << "***  目的     距离     下一跳  ***" <<"\r"<<"\n";
         for (int j = 1; j <= RouteNum+1; ++j) {
-            if (j != i) file<<"      "<< j << "        " << cost[i][j] << "          " << NextRoute[i][j] <<"       "<<"\r"<<"\n";
+            if (j != i) file<<"*      "<< j << "        " << cost[i][j] << "          " << NextRoute[i][j] <<"       "<<"\r"<<"\n";
         }
         file << "**********************************" <<"\r"<<"\n";
         file <<"\r"<<"\n";
